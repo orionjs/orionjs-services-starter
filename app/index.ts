4be551c1a0e54426c10d6e9902f8c268f02e449d
Nicolás López <nicolaslopezj@me.com>
2022-05-06 11:47:42 -0400

    new syntax for index and config start app

--- app/index.ts
+++ app/index.ts
@@ -1,20 +1,4 @@
-import './config/options'
-import startEchoes from './config/echoes'
-import startGraphQL from './config/graphql'
-import startHttp from './config/http'
-import startJobs from './config/jobs'
-import {mergeComponents} from '@orion-js/components'
+import {startApp} from './config'
 import exampleComponent from './exampleComponent'
 
-async function startApp() {
-  const {echoes, resolvers, modelResolvers, routes, jobs} = mergeComponents([
-    exampleComponent,
-  ])
-
-  startEchoes(echoes)
-  startGraphQL(resolvers, modelResolvers)
-  startHttp(routes)
-  startJobs(jobs)
-}
-
-startApp()
+startApp([exampleComponent])
